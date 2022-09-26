854bf71a71209a0bb30925a454b8e0a8c76983d3
Jakub Zuber <jakub.zuber19@gmail.com>
2022-09-26 23:16:48 +0200

    background added

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# Hello
+# Currency Calculator
 
+## What it do?
+
+User can check the value in other currency.
+
+## Instruction.
+
+1. Select one of three currencies that are available from which you want to check value.
+2. Provide value that you want to count from.
+3. Select one of thee currencies that are available to which you want to check value.
+
+![alt text for screen readers](/pic/markdown_screenshot.png "Screen shot of currency calculator").
+
+* the calculator is automatic
+## Demo
 https://jakubzuber.github.io/currency-converter-react/
+
+## Technologies used:
+- HTML
+- CSS
+- BEM
+- JavaScript
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 
 import './App.css';
+import "./style.css";
 
 function App() {
   return (
-    <div>
+    <div className="body">
+    <form className="form js-form">
+        <fieldset className="form__fieldset">
+            <legend className="form__title">Currency Calculator</legend>
+            <p>
+                <label>
+                    <span className="form__labelText">Conversion from:</span>
+                    <select name="currencyInput" required className="js-currencyInput form__field">
+                        <option value="pln">PLN</option>
+                        <option value="eur">EUR</option>
+                        <option value="gbp">GBP</option>
+                        <option value="hkd">HKD</option>
+                    </select>
+                </label>
+            </p>
+            
+                <label>
+                    <span className="form__labelText">Amount:*</span><input type="number"  className="js-amount form__field" required step="1" min="10" autoFocus placeholder="Provide amount"/>
+                </label>
+            <p>
+                <label><span className="form__labelText">Conversion to:</span>
+                    <select name="value" required className="js-currencyOutput form__field">
+                        <option value="pln">PLN</option>
+                        <option value="eur">EUR</option>
+                        <option value="gbp">GBP</option>
+                        <option value="hkd">HKD</option>
+                    </select>
+                </label>
+            </p>
+            <p><span className="form__labelText">Converted amount:</span><strong className="js-result form__result">N/A</strong></p>
+            <p className="form__annotation">Course from 9.09.2022</p>
+            <p className="form__annotation">*required fields</p>
+        </fieldset>
+    </form>
     </div>
   );
 }
