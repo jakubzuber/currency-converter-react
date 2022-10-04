5ddffb3f6be084ae27b43125f9f18db97b5b95e8
Jakub Zuber <jakub.zuber19@gmail.com>
2022-10-04 22:42:32 +0200

    time and date added

--- src/Form/index.js
+++ src/Form/index.js
@@ -5,8 +5,8 @@ const Form = ({ children, rateDate, renderDate, renderTime }) => (
     <form className="body">
         <fieldset className="form__fieldset">
             <legend className="form__title">Currency Calculator</legend>
-            <p>Current date: {renderDate}</p>
-            <p>Current hour: {renderTime}</p>
+            <p className="form_dateTime">Current date: {renderDate}</p>
+            <p className="form_dateTime">Current hour: {renderTime}</p>
             {children}
             <p className="form__annotation">Conversion rate from {rateDate}</p>
             <p className="form__annotation">*required fields</p>
